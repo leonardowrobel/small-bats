0cffe522cbf469a6f2b879e93bb884df5522dc07
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2021-05-07 16:56:23 -0300

    commit inicial

---
+++ src/main/java/br/com/smallbats/SmallBatsApplication.java
@@ -0,0 +1,13 @@
+package br.com.smallbats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBatsApplication.class, args);
+	}
+
+}
